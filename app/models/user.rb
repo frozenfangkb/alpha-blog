dda07d90f104a1eef244f283ea62b14057754ffd
David Rodriguez Miranda <david@MacBook-Air-de-David.local>
2020-03-25 19:30:40 +0100

    user model creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
-    
+    validates :username, uniqueness: { case_sensitive: false }, 
+    presence: true, length: {minimum: 3, maximum: 25}
+
+    VALID_EMAIL_REGEX = /\A([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})\z/i
+
+    validates :email, presence: true, length: { maximum: 105 },
+    uniqueness: { case_sensitive: false },
+    format: { with: VALID_EMAIL_REGEX }
+
 end
--- db/migrate/20200325179349_drop_tables.rb
+++
@@ -1,7 +0,0 @@
-class DropTables < ActiveRecord::Migration[6.0]
-  def change
-    remove_foreign_key :comments, :users
-    drop_table :users
-    drop_table :comments
-  end
-end
